7fc9d53ae0bd255bfe587b341a64c7069b7b836b
VanyaMark <markova.vanya@gmail.com>
2024-05-06 19:11:18 +0200

    ex 1 done

--- ex1.js
+++ ex1.js
@@ -7,25 +7,27 @@
 
 /**
  * Cambia el título de la página a rojo usando un estilo en línea
- */
+*/
+document.querySelector('h1').style.color = 'red'
 
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
- */
+*/
+document.querySelector('h1').style.fontSize='48px'
 
 /**
  * Muestra por console.log el texto de la caja 2
  */
-
+console.log(document.querySelector('#caja-2').textContent)
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
-
+document.querySelector('.intro').style.border = 'green 20px solid'
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
-
+document.querySelector('#ultimo').classList.remove('estilo-3')
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo
  * Ayuda: https://simpledev.io/lesson/add-class-js/
@@ -33,3 +35,4 @@
  * BONUS: Haz el ejercicio sin modificar el ex1.html
  
  */
+document.querySelector('#ultimo').classList.add('ocultar')
